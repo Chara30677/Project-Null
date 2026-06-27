9c14627f9de5d5eefaf9ec60a48f298378f020b7
Chara10163 <chara30678@gmail.com>
2026-06-27 17:02:48 -0400

    Set up minecraft instance

---
+++ src/main/java/com/projectnull/ProjectNull.java
@@ -0,0 +1,12 @@
+package com.projectnull;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(ProjectNull.MODID)
+public class ProjectNull {
+    public static final String MODID = "projectnull";
+
+    public ProjectNull(IEventBus modEventBus) {
+    }
+}
